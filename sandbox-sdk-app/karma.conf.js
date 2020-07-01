d730e36e54da139ee3ae41dfbf6d1badf0173371
Pawel Paulinski <pawel.paulinski.bc@gmail.com>
2020-07-01 09:14:07 +0200

    fix: browser

--- sandbox-sdk-app/karma.conf.js
+++ sandbox-sdk-app/karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ["ChromiumHeadless"],
+    browsers: ["ChromeHeadless"],
     singleRun: true,
     restartOnFileChange: false,
   });
