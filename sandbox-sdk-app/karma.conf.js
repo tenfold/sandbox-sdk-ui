96cacafaaf470a667d8728b41a5b1c17e521ab08
Pawel Paulinski <pawel.paulinski.bc@gmail.com>
2020-07-01 09:12:27 +0200

    fix: no puppeteer

--- sandbox-sdk-app/karma.conf.js
+++ sandbox-sdk-app/karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-const puppeteer = require("puppeteer");
-process.env.CHROMIUM_BIN = puppeteer.executablePath();
+// const puppeteer = require("puppeteer");
+// process.env.CHROMIUM_BIN = puppeteer.executablePath();
 
 module.exports = function (config) {
   config.set({
