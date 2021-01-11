62ea9c1fda072cae061cdeaee40f2fe7da51e3aa
Pawel Paulinski <pawel.paulinski.bc@gmail.com>
2021-01-11 14:27:10 +0100

    feat: cold transfer support

---
+++ sandbox-sdk-app/src/app/components/transfers-section/transfers-section.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TransfersSectionComponent } from './transfers-section.component';
+
+describe('TransfersSectionComponent', () => {
+  let component: TransfersSectionComponent;
+  let fixture: ComponentFixture<TransfersSectionComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TransfersSectionComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TransfersSectionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
